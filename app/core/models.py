56ca9029c29e12bb8b13401f211f84e2b33fa883
Gustavo Rosa <gustavorib.rosa@gmail.com>
2024-01-02 14:18:21 -0300

    User model implemented

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,34 @@
-from django.db import models
+"""
+Database models
+"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
+
+class UserManager(BaseUserManager):
+    
+    def create_user(self, email, password=None, **extra_fields):
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+
+# AbstactBaseUser contains the funcionality for auth system
+# PermissionsMixin contains the funcionality for permissions and fields
+class User(AbstractBaseUser, PermissionsMixin):
+
+    # User in the system
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
