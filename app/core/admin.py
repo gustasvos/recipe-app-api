a7bcc25e2468adc18b777d81f4c263fd2f44bc61
Gustavo Rosa <gustavorib.rosa@gmail.com>
2024-01-03 19:09:38 -0300

    Added support for modifying users via admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,5 +1,27 @@
 from django.contrib import admin
-from core.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
-admin.site.register(User)
-# Register your models here.
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
+
+admin.site.register(models.User, UserAdmin)
