3477a67722e60251d30888183b80280118b9e9d2
Gustavo Rosa <gustavorib.rosa@gmail.com>
2024-01-04 15:02:54 -0300

    Added create user API

--- app/user/views.py
+++ app/user/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+"""
+Views for the user API
+"""
 
-# Create your views here.
+from rest_framework import generics
+
+from user.serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+
+    serializer_class = UserSerializer
